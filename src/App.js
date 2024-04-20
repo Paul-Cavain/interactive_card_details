a44f71a884ffacbb70bc66a1e3e28b5b4413640f
Paul-Cavain <paulcavain01@gmail.com>
2024-04-20 23:55:36 +0300

    added usestate

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import Home from './Home'
 import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState('');
+  const [cardNumber, setCardNumber] = useState('');
+  const [month, setMonth] = useState('');
+  const [year, setYear] = useState('');
+  const [cvc, setCvc] = useState('');
 
   return(
     <section>
-      <Home />
+      <Home 
+        name = {name}
+        setName = {setName}
+        cardNumber = {cardNumber}
+        setCardNumber = {setCardNumber}
+        month = {month}
+        setMonth = {setMonth}
+        year = {year}
+        setYear = {setYear}
+        cvc = {cvc}
+        setCvc = {setCvc}
+
+      />
     </section>
   )
 }
